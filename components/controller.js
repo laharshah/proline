f63dbb71762aded990e0ea1367dc8d0f2c875e0e
Lahar Shah <shahlahar@outlook.com>
2015-05-18 17:05:42 +0000

    for grunt

---
+++ components/controller.js
@@ -0,0 +1,25 @@
+/**
+ * Controller for view tables page
+ */
+
+angular.module('prolineApp.ClubFitting', []);
+
+angular.module('prolineApp.ClubFitting').config(['$routeProvider', function($routeProvider) {
+				
+		$routeProvider
+			.when("/club-fitting", {
+				controller: 'ClubFittingController',
+				templateUrl: 'modules/club-fitting/view.html',
+			});
+	}
+]);
+
+angular.module('prolineApp').controller('ClubFittingController', function ($scope, $rootScope, $location, $cookieStore, authFactory) {
+	
+	$scope.pagename = "club-fitting";
+	$scope.infolist=[
+			{"heading":"Mizuno Shaft Optimizer", "contents":"this is details","leave":"button"},
+			{"heading":"Ping nFlight", "contents":"this is details","leave":"button"},
+			{"heading":"Flight Scope", "contents":"this is details","leave":"button"}
+		];
+});
---
+++ components/model.js
@@ -0,0 +1,14 @@
+/**
+ * Initializing the database model for login controller
+ * @type [angular module]
+ */
+	angular.module('prolineApp.ClubFitting').factory('club-fittingFactory', ['$http', function ($http) {
+		
+		/**
+		 * Blank authFactory
+		 * @type {Object}
+		 */
+		var authFactory = {};
+
+		return authFactory;
+	}]);
