014754faa50a43c61328220c2cf305506b1198e5
Lahar Shah <shahlahar@outlook.com>
2015-05-18 20:04:18 +0000

    grunt thing

--- front_end/app/modules/custom-products/controller.js
+++ front_end/app/modules/custom-products/controller.js
@@ -7,7 +7,7 @@ angular.module('prolineApp.CustomProducts', []);
 angular.module('prolineApp.CustomProducts').config(['$routeProvider', function($routeProvider) {
 				
 		$routeProvider
-			.when("/corporate-sales", {
+			.when("/custom-products", {
 				controller: 'CustomProductsController',
 				templateUrl: 'modules/custom-products/view.html',
 			});
@@ -17,4 +17,35 @@ angular.module('prolineApp.CustomProducts').config(['$routeProvider', function($
 angular.module('prolineApp').controller('CustomProductsController', function ($scope, $rootScope, $location, $cookieStore, authFactory) {
 	
 	$scope.pagename = "corporate-sales";
+	
+	$scope.customproducts = [
+  						{
+  							name:'Logo Golf Balls',
+  							about:'Details here of p1',
+  							imgpath: 'https://proline-laharshah.c9.io/front_end/app/assets/images/custom-products/large_thumb_logo_balls.jpg',
+  						  	custom_p_id:'custom_p_1'
+              			},
+              			
+              			{
+  							name:'Tournament Accessories',
+  							about:'Details here of p2',
+  							imgpath: 'https://proline-laharshah.c9.io/front_end/app/assets/images/custom-products/large_thumb_miniature-golf-bag-extralarge.jpg',
+  						  	custom_p_id:'custom_p_2'
+              			},
+              			
+              			{
+  							name:'Logo on Apperal',
+  							about:'Details here of p3',
+  							imgpath: 'https://proline-laharshah.c9.io/front_end/app/assets/images/custom-products/large_thumb_CIMG09383.jpg',
+  						  	custom_p_id:'custom_p_3'
+              			},
+              			
+              			{
+  							name:'Logo on Bags',
+  							about:'Details here of p4',
+  							imgpath: 'https://proline-laharshah.c9.io/front_end/app/assets/images/custom-products/large_thumb_scotland-golf-gear-extralarge.jpg',
+  						  	custom_p_id:'custom_p_4'
+              			}];
+	
+	
 });
